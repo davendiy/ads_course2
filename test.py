bde01405ffe502bb2ad85834a11ddc4cb14dad2c
David Zashkolny <davendiy@gmail.com>
2018-11-21 10:45:15 +0200

    dz

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*-encoding: utf-8-*-
+
+# by David Zashkolny
+# 2 course, comp math
+# Taras Shevchenko National University of Kyiv
+# email: davendiy@gmail.com
+# 19.11.18
