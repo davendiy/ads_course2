e5ce7cbe45406f3499157079a9ca33dc663d88c1
davidas <davendiy@gmail.com>
2019-03-28 00:08:45 +0200

    start project 7

---
+++ math_projects/alexandra/server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*-encoding: utf-8-*-
+
+# t27_01_local_webserver.py
+# Локальний веб-сервер
+
+# TODO дохрена ше нада всього зробити
+# TODO зробити теплейти таблиць на хтмл сторінках так, щоб скрипт міг
+# TODO додавати записи.
+
+# TODO між хтмл сторінками переходим по ссилкам, щоб зробити якісь зміни - формуємо запрос
+# TODO также нада додати діалогові сторінки, а ето жопа. ВЕРНІТЬ ПОЖАЛУСТА GUI
+
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+HOST = ''               # Комп'ютер для з'єднання
+PORT = 8000             # Порт для з'єднання
+
+print('=== Local webserver ===')
+print("стартова сторінка:", "http://localhost:{}/front/start_page.html".format(PORT))
+HTTPServer((HOST, PORT), CGIHTTPRequestHandler).serve_forever()
