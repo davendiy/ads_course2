32345eaf36f92bf0c70b9619ef985c03493bd1bf
David Zashkolny <davendiy@gmail.com>
2019-03-30 16:04:18 +0200

    configure __init__

--- math_projects/alexandra/cgi-bin/structure/__init__.py
+++ math_projects/alexandra/cgi-bin/structure/__init__.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 # -*-encoding: utf-8-*-
 
-# created: 30.03.2019
-# by David Zashkolny
-# 2 course, comp math
-# Taras Shevchenko National University of Kyiv
-# email: davendiy@gmail.com
+from .constants import *
+from .database import *
+
+__all__ = ['HOME_PAGE',
+           'HOME_PAGE_PARAMS',
+           'REVENUE',
+           'REVENUE_FIELDS',
+           'REVENUE_PAGE',
+           'COST',
+           'COSTS_FIELDS',
+           'COSTS_PAGE',
+           'BudgetDB',
+           'BudgetCollection',
+           'DEFAULT_DATABASE',
+           'DEFAULT_N',
+           ]
