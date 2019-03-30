1142d72c5cd9cf12858c43a7b3e5fd3387cef98e
davidas <davendiy@gmail.com>
2019-03-30 16:00:33 +0200

    strange __init__

--- math_projects/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*-encoding: utf-8-*-
-
-# created: 30.03.2019
-# by David Zashkolny
-# 2 course, comp math
-# Taras Shevchenko National University of Kyiv
-# email: davendiy@gmail.com
