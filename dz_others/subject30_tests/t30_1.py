cd69bfba9548b4a3bc9f6e4c98a283f337e9223e
davidas <davendiy@gmail.com>
2019-04-28 11:13:33 +0300

    unittest to doctest

--- dz_others/subject30_tests/t30_1.py
+++ dz_others/subject30_tests/t30_1.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python3
 # -*-encoding: utf-8-*-
 
-import unittest
-from math import log
-
 
 def logarithm(x, eps=10e-5):
+    """ Calculate log(x + 1), |x| < 1
 
+    >>> from math import log
+
+    >>> abs(logarithm(0.6) - log(1.6)) < 10e-5
+    True
+    >>> abs(logarithm(0.8) - log(1.8)) < 10e-5
+    True
+    >>> abs(logarithm(0.1) - log(1.1)) < 10e-5
+    True
+    >>> abs(logarithm(0.3) - log(1.3)) < 10e-5
+    True
+
+    >>> logarithm(2)
+    nan
+    >>> logarithm(-2.5)
+    nan
+    >>> logarithm(1.1)
+    nan
+
+    >>> abs(logarithm(0.2, 10e-6) - log(1.2)) < 10e-6
+    True
+    >>> abs(logarithm(0.2, 10e-2) - log(1.2)) < 10e-2
+    True
+    >>> abs(logarithm(0.2, 10e-4) - log(1.2)) < 10e-4
+    True
+
+    :param x: float number
+    :param eps: accurany of calculation
+    :return: float number
+    """
     if abs(x) >= 1:
         return float('Nan')
 
@@ -24,22 +51,3 @@ def logarithm(x, eps=10e-5):
         res_x += sign * tmp / i
 
     return res_x
-
-
-class Test(unittest.TestCase):
-
-    def test1(self):
-        self.assertTrue(abs(logarithm(0.6) - log(1.6)) < 10e-5, True)
-        self.assertTrue(abs(logarithm(0.8) - log(1.8)) < 10e-5, True)
-        self.assertTrue(abs(logarithm(0.1) - log(1.1)) < 10e-5, True)
-        self.assertTrue(abs(logarithm(0.3) - log(1.3)) < 10e-5, True)
-
-    def test2(self):
-        self.assertTrue(logarithm(2), float('Nan'))
-        self.assertTrue(logarithm(-2.5), float('Nan'))
-        self.assertTrue(logarithm(1.1), float('Nan'))
-
-    def test3(self):
-        self.assertTrue(abs(logarithm(0.2, 10e-6) - log(1.2)) < 10e-6, True)
-        self.assertTrue(abs(logarithm(0.2, 10e-2) - log(1.2)) < 10e-2, True)
-        self.assertTrue(abs(logarithm(0.2, 10e-4) - log(1.2)) < 10e-4, True)
